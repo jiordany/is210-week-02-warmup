8645bb27fed7758f3ebd30e54da770950c99ec9e
jiordany <angel.guerra@sps.cuny.edu>
2016-02-18 22:51:25 +0000

    updating python error

---
+++ interpreter directive.py
@@ -0,0 +1,9 @@
+Python 2.7.6 (default, Jun 22 2015, 18:00:18) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Hello World!
+>>> #!/usr/bin/env python
+>>> print Hello world!!!
+
